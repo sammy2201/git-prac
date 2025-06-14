086d303867a1fd21386a4a6ef94a036f89156683
sammy2201 <bhavanish2000@gmail.com>
2025-06-14 20:19:52 +0530

    Few changesa re made in src

--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -5,7 +5,11 @@
 //     let sut = isUppercase("hulk");
 //     expect(sut).toBe("HULK");
 //   });
-
+//changes
+//changes-2
+//changes-3
+//changes-4
+//chamges-5
 //   it.only("should return uppercase", () => {
 //     let sut = isUppercase("hulkK");
 //     expect(sut).toBe("HULKK");
